54b67d73d3c7345365ae4cf3e47cc078bb79a15a
escolateologicavance-debug <escolateologicavance@gmail.com>
2025-12-27 15:12:17 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'lapada-cache-v1';
+const assets = [
+  './',
+  './index.html',
+  './capa-512.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
